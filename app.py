a300af49c61500a2b227fbac7ea59a4234421200
Ganesh Chouhan <147220752+gangaram7877@users.noreply.github.com>
2025-01-29 12:04:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,3 +33,15 @@ def home():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Tech VJ'
+
+
+if __name__ == "__main__":
+    app.run()
+  
